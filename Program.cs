adf62617a18b0a1459ff8990e73afaca68946c54
Thaoden <Thaoden@users.noreply.github.com>
2019-10-12 23:50:32 +0200

    * Removed obsolete calculation step * Introduced debug flag for console output

--- Program.cs
+++ Program.cs
@@ -5,50 +5,24 @@ namespace MTGDraftCollectionCalculator
     class Program
     {
         private readonly static Random _rng = new Random();
+        private readonly static bool _debug = false;
 
         static void Main(string[] args)
         {
             var userCollection = createUserCollection();
             Console.WriteLine(userCollection.ToString());
 
-            calculateNonDraftableCollectionProgress(userCollection);
-            Console.WriteLine($"Owning {userCollection.BoosterPacksOwned} booster packs after non-draftable progression.{Environment.NewLine}");
+            int draftsNeeded = calculateDrafts(userCollection);
 
-            double draftsNeeded = calculateDrafts(userCollection);
-
-            Console.WriteLine($"Estimated runs to complete the rare collection: {Math.Floor(draftsNeeded)}");
+            Console.WriteLine($"Estimated runs to complete the rare collection: {draftsNeeded}");
+            Console.WriteLine(userCollection.ToString());
         }
 
-        private static void calculateNonDraftableCollectionProgress(UserCollection userCollection)
+        private static int calculateDrafts(UserCollection userCollection)
         {
-            Console.WriteLine($"Using {userCollection.BoosterPacksOwned} packs to advance the non-draftable collection...");
+            int draftsNeeded = 0;
 
-            if (userCollection.Rares.NonDraftablesNeeded > userCollection.BoosterPacksOwned)
-            {
-                // we don't have enough boosters to complete the non-draftableCollection
-                // -> use all available booster as non-draftable collection progress
-                userCollection.Rares.NonDraftablesOwned += userCollection.BoosterPacksOwned;
-                userCollection.BoosterPacksOwned = 0;
-
-                Console.WriteLine($"... still need {userCollection.Rares.NonDraftablesNeeded - userCollection.BoosterPacksOwned} non-draftable rare(s).");
-            }
-            else
-            {
-                // we have enough boosters to complete the non-draftable collection
-                // -> use all necessary boosters as non-draftable collection progress, keep the remaining ones
-                userCollection.BoosterPacksOwned -= userCollection.Rares.NonDraftablesNeeded;
-                userCollection.Rares.NonDraftablesOwned = SetCollection.Eldraine.Rares.NonDraftable;
-
-                Console.WriteLine($"Enough packs to complete the non-draftable collection :)");
-            }
-        }
-
-
-        private static double calculateDrafts(UserCollection userCollection)
-        {
-            var draftsNeeded = 0.0;
-
-            while (userCollection.Rares.DraftablesNeeded > userCollection.BoosterPacksOwned)
+            while (userCollection.Rares.DraftablesNeeded + userCollection.Rares.NonDraftablesNeeded >= userCollection.BoosterPacksOwned)
             {
                 simulateSingleDraft(userCollection);
                 draftsNeeded++;
@@ -69,6 +43,11 @@ namespace MTGDraftCollectionCalculator
         private static void simulateSinglePack(UserCollection userCollection)
         {
             var rnd = _rng.Next(SetCollection.Eldraine.Rares.Draftable);
+
+            if (_debug)
+            {
+                Console.WriteLine($"Current number of owned draftables: {userCollection.Rares.DraftablesOwned}{Environment.NewLine}Opened rare number {rnd}");
+            }
 
             if (rnd > userCollection.Rares.DraftablesOwned)
             {
